39ca0bea40f0e3dcb04de417d3b37fdc46c746c0
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-28 01:52:03 -0500

    Add Characterstats to prototype.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -33,6 +33,17 @@ GameObject.prototype.destroy = function(){
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(charAttr){
+  GameObject.call(this, charAttr);
+  this.healthPoints = charAttr;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage`;
+};
+
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
