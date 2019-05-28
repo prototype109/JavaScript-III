5b428448370102b23db92bd840d52af53fea5e4c
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-27 22:25:37 -0500

    Finsihed principle 1 and 2 of this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. "this" keyword is bound to the forest which is whatever the root is of the environment being used.
+* 2. "this" keyword is bound to whatever is on the left of the dot operator. It is implicitely known what "this" refers to.
 * 3. 
 * 4. 
 *
@@ -10,11 +10,21 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
+function Person(greet, name){
+    this.greeting = this.greet;
+    this.myName = this.name;
+    this.sayHi = function(){
+        console.log(`${this.greeting} ${this.myName}`);
+    };
+}
+
+matilda.sayHi('Hi my name is', 'Matilda');
+Fred.sayHi('Hello there', 'General Kenobi');
 // code example for Implicit Binding
 
 // Principle 3
